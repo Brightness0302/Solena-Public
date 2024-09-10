db786befb136d8d240af486e0ae6d9b1dd8d8cc6
brightness0302 <brightnes0302@gmail.com>
2024-09-10 11:33:16 -0700

    fix initial route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import {
   ConnectionProvider,
   WalletProvider,
@@ -42,7 +42,7 @@ const App = () => {
           <BrowserRouter>
             <Routes>
               <Route exact path="/" element={<Layout />}>
-                <Route exact index element={<Home />} />
+                <Route path="/" element={<Navigate to="/swap" replace />} />
                 <Route
                   path="stake/sol"
                   element={downMd ? <Stake /> : <Home />}
